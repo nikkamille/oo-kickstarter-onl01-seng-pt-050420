60301adc23aa27c52219a8ed716e86a1a57b79c0
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-04 05:26:55 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,7 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(project)
+  def back_project(project) # also adds the backer to the project\'s backers array
     @backed_projects << project 
   end
   
